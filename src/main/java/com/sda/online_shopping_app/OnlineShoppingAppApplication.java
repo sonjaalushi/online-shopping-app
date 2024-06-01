244bee566bb34bd71226d4b37fceb082d2aee381
Alushi, Sonja <sonja.alushi@lhind.dlh.de>
2024-06-01 10:00:40 +0200

    Initial commit

---
+++ src/main/java/com/sda/online_shopping_app/OnlineShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.sda.online_shopping_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/online_shopping_app/OnlineShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.online_shopping_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
