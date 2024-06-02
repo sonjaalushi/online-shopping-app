92cb9de1940718cee51f7afa2ced4228b1796e4e
renaldo000 <roniii2017@icloud.com>
2024-06-03 00:44:41 +0200

    .

---
+++ src/main/java/com/sda/online_shopping_app/repo/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.sda.online_shopping_app.repo;
+
+import com.sda.online_shopping_app.entity.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProductRepo extends JpaRepository<Products,Integer> {
+}
