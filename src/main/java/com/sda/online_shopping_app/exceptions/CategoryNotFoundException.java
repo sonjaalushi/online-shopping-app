d99356ee90b5417a3a2ddc684828c049ae110a92
renaldo000 <roniii2017@icloud.com>
2024-06-03 15:23:55 +0200

    add Products Controller.

--- src/main/java/com/sda/online_shopping_app/exceptions/CategoryNotFoundException.java
+++ src/main/java/com/sda/online_shopping_app/exceptions/CategoryNotFoundException.java
@@ -1,6 +1,7 @@
 package com.sda.online_shopping_app.exceptions;
 
 public class CategoryNotFoundException extends RuntimeException {
+
     public CategoryNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/sda/online_shopping_app/service/ProductsService.java
+++ src/main/java/com/sda/online_shopping_app/service/ProductsService.java
@@ -1,6 +1,7 @@
 package com.sda.online_shopping_app.service;
 
 
+import com.sda.online_shopping_app.entity.Products;
 import com.sda.online_shopping_app.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,13 @@ public class ProductsService {
 
     @Autowired
     ProductRepo productRepo;
+
+
+    public Products save(Products products) {
+       return productRepo.save(products);
+    }
+
+    public void delete(Products products){
+         productRepo.delete(products);
+    }
 }
