af8333bd0b395e9f1112ccdd251d478d35fa906c
renaldo000 <roniii2017@icloud.com>
2024-06-22 18:13:15 +0200

    ....

--- src/main/java/com/sda/online_shopping_app/service/UserService.java
+++ src/main/java/com/sda/online_shopping_app/service/UserService.java
@@ -4,6 +4,7 @@ import com.sda.online_shopping_app.entity.User;
 import com.sda.online_shopping_app.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -38,27 +39,27 @@ public class UserService {
     }
 
 
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    public User registerUser(User user) {
-//        if (userRepository.getByEmail(user.getEmail()).isPresent()) {
-//            throw new RuntimeException("Email already in use");
-//        }
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
-//        return userRepository.save(user);
-//    }
-//
-//    public Optional<User> loginUser(String email, String password) {
-//        Optional<User> userOptional = userRepository.getByEmail(email);
-//        if (userOptional.isPresent()) {
-//            User user = userOptional.get();
-//            if (passwordEncoder.matches(password, user.getPassword())) {
-//                return Optional.of(user);
-//            }
-//        }
-//        return Optional.empty();
-//    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        if (userRepository.getByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already in use");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> loginUser(String email, String password) {
+        Optional<User> userOptional = userRepository.getByEmail(email);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
 
 
 }
